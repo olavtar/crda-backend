fe6df6f21ce94210420d854c30b5c2db4ba1bb76
Olga Lavtar <olavtar@redhat.com>
2023-10-24 13:56:27 -0700

    Init commit
    Signed-off-by: Olga Lavtar <olavtar@redhat.com>

--- ui/src/App.test.tsx
+++ ui/src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Red Hat Overview of security Issues/i);
   expect(linkElement).toBeInTheDocument();
 });
